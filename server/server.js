21ec1a6ac599c1b187f2472dd7677d8a3405f3e4
Mahalingam <vmahalingam@deloitte.com>
2018-06-06 18:48:56 +0530

    rest apis

---
+++ server/server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+
+var{mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+
+app.post('/todos',(req,res)=>{
+
+  var todo = new Todo({
+    text : req.body.text
+  })
+
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(e)=>{
+    res.send(e);
+  })
+console.log(req.body);
+});
+
+app.listen(3000,()=>{
+  console.log("started");
+
+});

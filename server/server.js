4ec857215aabbebf861cafb45320e0f3f5a32a4b
Mahalingam <vmahalingam@deloitte.com>
2018-06-07 19:20:28 +0530

    with db queries

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
+
 
 
 var{mongoose} = require('./db/mongoose');
@@ -25,6 +27,25 @@ app.post('/todos',(req,res)=>{
   })
 console.log(req.body);
 });
+
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({
+      todos
+    })
+  },(e)=>{
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+  return  res.status(404).send();
+  }
+  Todo.findById(id).then((todo)=>{res.send(todo)},(e)=>{res.status(400).send();}).catch((e)=>{res.status(400).send();});
+
+})
 
 app.listen(3000,()=>{
   console.log("started");

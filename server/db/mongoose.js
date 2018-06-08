db7cf2c2fc9b80af45d086107b1c006db2aec7d3
Mahalingam <vmahalingam@deloitte.com>
2018-06-08 17:30:33 +0530

    update api

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose  = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://todos1:todos1@ds153460.mlab.com:53460/todoapp');
+mongoose.connect('mongodb://todos1:todos1@ds153460.mlab.com:53460/todoapp'||'mongodb//localhost:27017/TodoApp');;
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -5,7 +7,7 @@ var {ObjectID} = require('mongodb');
 
 
 
-var{mongoose} = require('./db/mongoose');
+var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -48,7 +50,33 @@ app.get('/todos/:id',(req,res)=>{
   }
   Todo.findById(id).then((todo)=>{res.send(todo)},(e)=>{res.status(400).send();}).catch((e)=>{res.status(400).send();});
 
+});
+
+app.patch('/todos/:id',(req,res)=>{
+var id = req.params.id;
+var body = _.pick(req.body,['text','completed']);
+
+if(!ObjectID.isValid(id)){
+return  res.status(404).send();
+}
+if(_.isBoolean(body.completed)&&body.completed){
+  body.completedAt = new Date().getTime();
+}
+else{
+  body.completed = false;
+  body.completedAt = null
+}
+
+Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+  if(!todo){
+    return res.status(404).send();
+  }
+  res.send({todo});
+}).catch((e)=>{
+  return res.status(404).send();
 })
+});
+
 
 app.listen(port,()=>{
   console.log(`started at port ${port}`);
